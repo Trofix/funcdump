381c88c8564386d3f341ee72b320661fdfc023d4
moriczgergo <moriczgergo05@gmail.com>
2016-12-28 17:45:02 +0100

    Added

---
+++ funcdump/Parser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace funcdump
+{
+    class Parser
+    {
+        private static string[] filelines;
+        private static string file;
+
+        public static void Start(string _file)
+        {
+            file = _file;
+            filelines = File.ReadAllLines(file);
+            
+        }
+
+        private static string[] SupArrSearch(string[] array, string searchTerm) //SUPARRSEARCH!(get it?!)
+        {
+            List<String> results = new List<String>();
+            foreach (string line in array)
+            {
+                if (line.IndexOf(searchTerm) != 0)
+                {
+                    results.Add(line);
+                }
+            }
+            return results.ToArray();
+        }
+    }
+}
---
+++ funcdump/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using funcdump;
+
+namespace funcdump
+{
+    class Program
+    {
+        static string path;
+        static string[] filelines;
+
+        static void Main(string[] args)
+        {
+            Parser.Start(filepath(args)); //Start parser
+        }
+
+        static string filepath(string[] args)
+        {
+            if (args.Length == 0) //If no args...
+            {
+                Console.Write("Enter file path: "); //...then ask for file path.
+                return Console.ReadLine(); //Load file path into path variable, since we can't load into args[] duh
+            }
+            else //If file path was specified in args...
+            {
+                return args[0]; //..then load file path into path variable.
+            }
+        }
+    }
+}
